ca9a122b1fa5217f897faa8afa8806a78446b7b2
mariam adly <mariam.adly@gmail.com>
2022-12-25 13:42:16 +0200

    add file hello and test

--- HelloITI.java
+++ HelloITI.java
@@ -1,6 +1,10 @@
 package tsk1;
 public class HelloITI
 {
+	
+	public void helloGit(){
+	System.out.println("hello git");	
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("hello ITI");
